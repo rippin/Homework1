0d2d445624032e215c034302bf519e5668092ac0
rippin <efisrippin@ive.com>
2015-09-29 19:17:11 -0700

    Initial Commit

---
+++ Homework1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Homework1
+//
+//  Created by Eric Franco on 9/29/15.
+//  Copyright (c) 2015 Eric Franco. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
